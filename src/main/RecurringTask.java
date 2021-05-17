7a42b3503b6a58fdc3b0e90fe46d92ed7ddf6a12
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2021-05-17 15:00:56 -0700

    Fixed RecurringTask to not have StartDate <= EndDate

--- src/main/RecurringTask.java
+++ src/main/RecurringTask.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.json.JSONObject;
+
 /**
  *
  */
@@ -8,6 +9,7 @@ public class RecurringTask extends Task {
 	public static String[] types = { "Class", "Study", "Sleep", "Exercise", "Work", "Meal" };
 	private int endDate;
 	private int frequency;
+
 	/**
 	 *
 	 * @param startTime
@@ -20,7 +22,8 @@ public class RecurringTask extends Task {
 	 * @throws Exception
 	 */
 
-	public RecurringTask(String name, float startTime, float duration, int date, String tname, int end, int freq) throws Exception {
+	public RecurringTask(String name, float startTime, float duration, int date, String tname, int end, int freq)
+			throws Exception {
 		super(name, startTime, duration, date, tname, Task.TaskType.RECURRING);
 		// Make sure the type matches what this Task can take.
 		if (!PSS.isIn(types, tname)) {
@@ -44,7 +47,6 @@ public class RecurringTask extends Task {
 		setFrequency(handler);
 	}
 
-
 	///////////////////////////// UserHandler Setters /////////////////////////////
 
 	public void setEndDate(UserHandler handler) {
@@ -56,64 +58,72 @@ public class RecurringTask extends Task {
 		int day;
 		int startDate = getDate();
 
-		do{
-			//Check if date is correct Length for formatting
-			if (sDate.length() == 8) {
-				//Check if it has a valid month
-				month = Integer.parseInt(sDate.substring(4, 6));
-				if (month <= 12 && month >= 1) {
-					//Check if day is valid
-					day = Integer.parseInt(sDate.substring(6, 8));
+		do {
+			if (date > startDate) {
 
-					int lastDayOfMonth = dayInMonth[month - 1];
-					if (day <= lastDayOfMonth && day >= 1) {
-						date = Integer.parseInt(sDate);
-						valid = true;
+				//Check if date is correct Length for formatting
+				if (sDate.length() == 8) {
+					//Check if it has a valid month
+					month = Integer.parseInt(sDate.substring(4, 6));
+					if (month <= 12 && month >= 1) {
+						//Check if day is valid
+						day = Integer.parseInt(sDate.substring(6, 8));
+
+						int lastDayOfMonth = dayInMonth[month - 1];
+						if (day <= lastDayOfMonth && day >= 1) {
+							date = Integer.parseInt(sDate);
+							// Check if end date is the same or larger than starting date
+							valid = true;
+
+						} else {
+							System.out.println("Invalid Day, Please enter a day that falls in your month");
+							sDate = handler.getLine();
+						}
 					} else {
-						System.out.println("Invalid Day, Please enter a day that falls in your month");
+						System.out.println("Invalid Month, Please enter a date with a month between 1-12");
 						sDate = handler.getLine();
 					}
 				} else {
-					System.out.println("Invalid Month, Please enter a date with a month between 1-12");
+					System.out.println("Incorrect Format, Please enter a new date (YYYYMMDD)");
 					sDate = handler.getLine();
 				}
 			} else {
-				System.out.println("Incorrect Format, Please enter a new date (YYYYMMDD)");
-				sDate = handler.getLine();
+				System.out.println("End day cannot be before or the same as the start day, please enter a new day");
+				date = handler.getInt();
 			}
-		}while(!valid && date>startDate);
+		} while (!valid && date <= startDate);
 
-		this.endDate=date;
+		this.endDate = date;
 	}
 
 	public void setFrequency(UserHandler handler) {
 		int freq = handler.getInt();
 		boolean valid = false;
 
-		while(!valid) {
+		while (!valid) {
 			// Check if length of frequency is valid
-			if (freq==1 || freq ==7){
+			if (freq == 1 || freq == 7) {
 				valid = true;
 			} else {
-				System.out.println("Invalid Frequency, Please enter a frequency equal to 1(once a day) of 7(every week)");
+				System.out
+						.println("Invalid Frequency, Please enter a frequency equal to 1(once a day) of 7(every week)");
 				freq = handler.getInt();
 			}
 		}
 		this.frequency = freq;
 	}
 
-
 	///////////////////////////// Code Setters /////////////////////////////
 
 	public void setEndDate(int endDate) throws Exception {
 		String sDate = String.valueOf(endDate);
-		int[] dayInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int[] dayInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		int month;
 		int day;
 		int startDate = getDate();
 
 		// Check if end date is the same or larger than starting date
-		if (endDate > startDate){
+		if (endDate > startDate) {
 			//Check if date is correct Length for formatting
 			if (sDate.length() == 8) {
 				//Check if it has a valid month
@@ -134,7 +144,7 @@ public class RecurringTask extends Task {
 				throw new Exception("Incorrect Format for Date");
 			}
 		} else {
-			throw new Exception("Incorrect End Date, is not equal to or greater than Start Date");
+			throw new Exception("Incorrect End Date, is not greater than Start Date");
 		}
 		this.endDate = endDate;
 	}
@@ -144,7 +154,7 @@ public class RecurringTask extends Task {
 		setDate(startDate);
 	}
 
-	public void setFrequency(int frequency)throws Exception{
+	public void setFrequency(int frequency) throws Exception {
 		// frequency doesn't equal 1 or 7, give error
 		if (frequency != 1 && frequency != 7)
 			throw new Exception("Invalid Frequency, needs to be frequency equal to 1(once a day) of 7(every week)");
@@ -182,13 +192,13 @@ public class RecurringTask extends Task {
 	@Override
 	public void print() {
 		float endTime = getStartTime() + getDuration();
-		if(endTime >= 24) {
+		if (endTime >= 24) {
 			endTime = endTime - 24;
 		}
-		System.out.println("Name: " + getName() + "\nType: " + getTypeName() + "\nStart Date: "
-				+ dateToHumanReadable(getDate()) + "\nTime: " + timeToHumanReadable(getStartTime())
-				+ " - " + timeToHumanReadable(endTime) + "\nEnd Date: "
-				+ dateToHumanReadable(getEndDate()) + "\nFrequency: " + getFrequency());
+		System.out.println(
+				"Name: " + getName() + "\nType: " + getTypeName() + "\nStart Date: " + dateToHumanReadable(getDate())
+						+ "\nTime: " + timeToHumanReadable(getStartTime()) + " - " + timeToHumanReadable(endTime)
+						+ "\nEnd Date: " + dateToHumanReadable(getEndDate()) + "\nFrequency: " + getFrequency());
 	}
 
 }
